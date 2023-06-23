fd798d28f69891a6a4c22b22a16fa4c82fa961b2
Joao Mendes <joaosilva0721@gmail.com>
2023-06-23 15:42:36 -0300

    implementando a função para obter os dados da API e atualizar o estado

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+import GameList from "../../components/GameList";
+import Loader from "../../components/Loader";
+import SearchInput from "../../components/SearchInput";
+import GenreFilter from "../../components/GenreFilter";
 
 
-import {Container} from './style'
+import {Container,ErrorMessage} from './style'
 import axios from "axios";
 
 const API_BASE_URL = "https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/";
@@ -10,25 +15,18 @@ const headers = {
     'dev-email-address': 'joaosilva0721@gmail.com',
   };
 
+ 
   
-
 const Home: React.FC = () => {
     
-    useEffect (() => {
-           axios.get(API_BASE_URL, {headers} )
-           .then(response => {
-            console.log(response.data);
-         })
-         .catch(error => {
-            console.error(error)
-        })
-        
-    }, [])
+
+    
   
     return (
       <Container>
-        <h1> Hello world</h1>
-      </Container>
+     <h1>ola mundo</h1>
+      
+    </Container>
     );
   };
   
--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components'
 
 
-export const Container = styled.div `
-`
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 16px;
+`;
+
+export const ErrorMessage = styled.p`
+  color: red;
+  font-weight: bold;
+`;
