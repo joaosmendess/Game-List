42df24304130796000d7a92cbc85f79f9b8c5e53
Joao Mendes <joaosilva0721@gmail.com>
2023-07-12 23:44:16 -0300

    testando rota

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,7 +10,6 @@ import Register from "../Pages/Register";
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login UserEmail={""} UserPassword={""} />} />
       <Route path="/register" element={<Register  UserEmail={""} UserPassword={""} />} />
       <Route path="/home" element={<Home />} /> {/* Usar o wrapper de rota para verificar a autenticação */}
